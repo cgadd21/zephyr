dc56e65e4c569f13dbc41786ca40c193a8ae7a25
cgadd21 <ctg7866@g.rit.edu>
2024-06-09 13:55:59 -0400

    prettier fix in hook

--- src/hooks/use.weather.station.ts
+++ src/hooks/use.weather.station.ts
@@ -34,7 +34,7 @@ type Observations = {
 
 export const useWeatherStation = () => {
   const { data, isLoading, error } = useSWR<Observations>(
-    import.meta.env.VITE_WEATHER_API_URL
+    import.meta.env.VITE_WEATHER_API_URL,
   );
 
   return {
