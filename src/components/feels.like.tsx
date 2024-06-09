fe980edbe3bc7017458a955596f0caf91ce13d19
cgadd21 <ctg7866@g.rit.edu>
2024-06-09 13:46:15 -0400

    Refactor FeelsLike component and remove unused code

--- src/components/feels.like.tsx
+++ src/components/feels.like.tsx
@@ -4,17 +4,14 @@ import { ThermometerLow } from "./icons/thermometer.low";
 import { Stats } from "./shared/stats";
 
 type FeelsLikeProps = {
-  humidity: number;
   imperial: {
     temp: number;
     heatIndex: number;
-    windSpeed: number;
   };
 };
 
 export const FeelsLike = (props: FeelsLikeProps) => {
-  const { humidity } = props;
-  const { temp, heatIndex, windSpeed } = props.imperial;
+  const { temp, heatIndex } = props.imperial;
 
   const figure =
     heatIndex > 80 ? (
@@ -27,9 +24,9 @@ export const FeelsLike = (props: FeelsLikeProps) => {
 
   const desc =
     heatIndex > temp
-      ? `With a wind speed of ${windSpeed} mph and humidity of ${humidity}%, it feels like ${heatIndex}°F.`
+      ? `Humidity is making it feel warmer.`
       : heatIndex < temp
-        ? `Despite a wind speed of ${windSpeed} mph and humidity of ${humidity}%, it feels like ${heatIndex}°F.`
+        ? `Wind is making it feel cooler.`
         : `Similar to the actual temperature.`;
 
   return (
