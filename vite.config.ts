9e1c431252febe10e5570d715089de0ef8239bce
cgadd21 <ctg7866@g.rit.edu>
2024-06-18 23:59:38 -0400

    Refactor Vite configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  // Load env variables based on the current mode
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    base: "/zephyr/",
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/zephyr/",
 });
